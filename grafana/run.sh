dc8b3a5256146d8369f4f7ff642c4a163d623e1b
Tony DiCola <tony@tonydicola.com>
2019-02-25 19:38:11 -0800

    Working builder image.

--- grafana/run.sh
+++ grafana/run.sh
@@ -8,13 +8,9 @@ set -e
 
 cd /opt/grafana-6.0.0
 
-echo 'run.sh: Replacing environment variables in provisioning configs.'
-echo 'run.sh: TEMPLATE CONFIG'
-cat /opt/grafana-6.0.0/conf/provisioning/datasources/influxdb.yml.template
-envsubst < /opt/grafana-6.0.0/conf/provisioning/datasources/influxdb.yml.template > /opt/grafana-6.0.0/conf/provisioning/datasources/influxdb.yml
-#find ./conf/provisioning -name '*.yml' -exec sh -c 'envsubst < {} > {}' \;
-echo 'run.sh: UPDATED CONFIG'
-cat /opt/grafana-6.0.0/conf/provisioning/datasources/influxdb.yml
+echo 'run.sh: Replacing environment variables in provisioning config.'
+envsubst < /opt/grafana-6.0.0/conf/provisioning/datasources/influxdb.yml.template \
+         > /opt/grafana-6.0.0/conf/provisioning/datasources/influxdb.yml
 
 ./bin/grafana-server web \
   cfg:default.log.mode="console" \
